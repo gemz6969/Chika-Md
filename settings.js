ebe5f638952cb26f34a96313e13cd93595a9eb01
garanganesia <84157263+garanganesia@users.noreply.github.com>
2022-05-27 00:45:01 +0700

    Add files via upload

--- settings.js
+++ settings.js
@@ -1,64 +1,105 @@
+/*
+› Create By @RIY
+› Base Ori @DikaArdnt
+› Kalau mau tambahin fitur di index.js
+› Edit text menu di file language › help.js
+*/
+
+//gausah di apa² in!
 const fs = require('fs')
 const chalk = require('chalk')
 
-// Website Api
+//—————「 Website Apikey 」—————//
 global.APIs = {
-	alfa: 'https://api.zeeoneofc.xyz', //apabila link api eror, segera laporkan le owner
+//gausah di apa² in!
+zenz: 'https://zenzapis.xyz',
 }
 
-// Free apikey
+//—————「 Website Apikey 」—————//
 global.APIKeys = {
-	'https://api.zeeoneofc.xyz': 'your apikey',  // login https://api.zeeoneofc.xyz to get apikey || https://api-alphabot.herokuapp.com
+//gausah di apa² in, udah free apikey :)
+'https://zenzapis.xyz': 'BagasPrdn',
 }
 
-// setting 
-global.autoread = false // auto read pesan / message
-//global.autorecording = true //status auto merekam ( auto record )
-//global.autoketik = false //status auto mengetik (auto typing)
-//global.available = false //status online (online)
+//—————「 Setting Owner 」—————//
+global.owner = ['6281263409730','6281263409730','6281263409730'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.premium = ['6281263409730'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.ownernomer = '6281263409730' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
-// Other
-global.botname = "Xdit-MD" //namabot kalian
-global.ownername= "® Raditya-¡X" //nama kalian
-global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
-global.github = "https://github.com/Xdit133" //bebas
-global.email = "rya76438@gmail.com" //bebas
-global.region = "Indonesia" //bebas
-global.ownernomer = "6285641476033" // nomor wa kalian
-global.ownernomerr = "+6285641476033" //nmr wa kalian
-global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
-global.donasi = "./image/donasi.jpg" // foto donasi di folder image
-global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
-global.owner = ["6285641476033","6285641476033","6285641476033"] //ganti agar fitur owner bisa di gunakan
-global.packname = 'Di Buat Oleh Xdit-MD' //sticker wm ubah
-global.author = '© Insta @hai_om.qw ' //sticker wm ganti nama kalian
+//—————「 Set Nama Own & Bot 」—————//
+global.ownername = 'HAKIM' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.botname = 'KIM BOTZ' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.footer = 'HAKIM' //ubah jadi nama mu, note tanda ' gausah di hapus!
+
+//—————「 Set Kebutuhan Button & Kontak 」—————//
+//ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
+global.youtube = '~'
+global.ig = '~'
+global.mygc = 'https://chat.whatsapp.com/CRY23lVKmXB7sCMhHmcwVw'
+global.myweb = '~'
+global.email = 'lalahakim095@gmail.com'
+global.region = 'indonesia'
+
+//—————「 Set Wm 」—————//
+global.packname = '© KIM BOTZ' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.author = 'HAKIM' //ubah jadi nama mu, note tanda ' gausah di hapus!
+
+//—————「 Set Nama Session 」—————//
+//gausah di apa² in!
 global.sessionName = 'session'
+
+//—————「 Set Limit 」—————//
+//terserah mau ubah atau nggak, note : tanda ' gausah di hapus!
+global.limitawal = {
+premium: "Infinity",
+free: 15,
+}
+
+//—————「 Set Image 」—————//
+//terserah mau ubah apa nggak, menurut gua jangan di ubah!
+global.thumb = fs.readFileSync('./image/chika.jpg')
+/*global.veloriy = fs.readFileSync('./image/chika.mp4')*/
+
+//—————「 Set Image Welcome 」—————//
+//terserah mau ubah apa nggak, menurut gua jangan di ubah!
+global.BgWelcLeav = 'https://telegra.ph/file/ca207893ae26d531cd9c6.jpg'
+
+//—————「 Set Random Image Menu 」—————//
+//gausah di apa² in!
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 
+//—————「 Set Prefix 」—————//
+//gausah di apa² in!
 global.prefa = ['','!','.','🐦','🐤','🗿']
+
+//—————「 Set Simbol 」—————//
+//terserah mai ubah atau nggak, note : tanda ' gausah di hapus!
 global.sp = '⭔'
+
+//—————「 Set Message 」—————//
+//terserah mau ubah apa nggak, note : tanda ' gausah di hapus!
 global.mess = {
-    sukses: '🤗Done, Oke Desu~',
-    admin: 'Perintah Ini Hanya Bisa Digunakan Oleh Admin !',
-    botAdmin: 'Perintah Ini Hanya Bisa Digunakan Ketika Bot Menjadi Admin !',
-    owner: 'Perintah Ini Hanya Bisa Digunakan Oleh Owner !',
-    group: 'Perintah Ini Hanya Bisa Digunakan Di Group !',
-    private: 'Perintah Ini Hanya Bisa Digunakan Di Private !',
-	bot: 'Fitur Khusus Pengguna Nomor Bot',
-    errtoimg: 'Maaf Saat Ini Belum Support Sticker Gif !',
-    wait: '⏳ Sedang Di Proses',
-	lockCmd: 'Fitur Tidak Diaktifkan Oleh Owner!',
-	example1: 'Selamat Datang @user Di Group @subject Jangan Lupa Baca Rules @desc\n\nNote :\n1. @user (Mention User Join)\n2. @bio (Get Bio User Join)\n3. @tanggal (Date Now)\n4. @desc (Get Description Group)\n5. @subject (Group Name)'
+success: '_Done._',
+admin: '_Fitur Khusus Admin Group!_',
+botAdmin: '_Bot Harus Menjadi Admin Terlebih Dahulu!_',
+owner: '_Fitur Khusus Owner Bot_',
+group: '_Fitur Digunakan Hanya Untuk Group!_',
+private: '_Fitur Digunakan Hanya Untuk Private Chat!_',
+bot: '_Fitur Khusus Pengguna Nomor Bot_',
+wait: '_Sedang Di Proses..._',
+error: '_Fitur Sedang Error!_',
+endLimit: '_Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12_',
 }
 
+//—————「 Batas Akhir 」—————//
+//gausah si apa² in!
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
-	fs.unwatchFile(file)
-	console.log(chalk.redBright(`Update'${__filename}'`))
-	delete require.cache[file]
-	require(file)
+fs.unwatchFile(file)
+console.log(chalk.redBright(`Update'${__filename}'`))
+delete require.cache[file]
+require(file)
 })
