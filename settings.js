5fa3b585996740b234cfde81927f3df54b694922
garanganesia <84157263+garanganesia@users.noreply.github.com>
2022-05-29 23:21:35 +0700

    Add files via upload

--- settings.js
+++ settings.js
@@ -1,86 +1,72 @@
 /*
-› Create By @RIY
-› Base Ori @DikaArdnt
-› Kalau mau tambahin fitur di index.js
-› Edit text menu di file language › help.js
+≈ Create By @RIY
+≈ Base Ori @DikaArdnt
 */
 
-//gausah di apa² in!
 const fs = require('fs')
 const chalk = require('chalk')
 
 //—————「 Website Apikey 」—————//
 global.APIs = {
-//gausah di apa² in!
 zenz: 'https://zenzapis.xyz',
 }
 
 //—————「 Website Apikey 」—————//
 global.APIKeys = {
-//gausah di apa² in, udah free apikey :)
-'https://zenzapis.xyz': 'bagasganz',
+'https://zenzapis.xyz': 'BagasPrdn',
 }
 
 //—————「 Setting Owner 」—————//
-global.owner = ['6285939243969','6285939243969','6285939243969'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.premium = ['6285939243969'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.ownernomer = '6285939243969' //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.owner = ["6281313915758','6281313915758','6281313915758']
+global.premium = ['6281313915758']
+global.ownernomer = '6281313915758'
 
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'BAGAS JB¹²' //ubah jadi nama mu, note tanda ' gausah di hapus!
-global.botname = 'BOTZ GASZ' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.footer = 'BAGAS JB¹²' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = 'Arwes'
+global.botname = 'Kurokawa Zaka'
+global.footer = 'Arwes'
 
 //—————「 Set Kebutuhan Button & Kontak 」—————//
-//ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
-global.youtube = 'https://youtube.com/channel/UCtM-pDaaGVRe4BJ7w4qE4Bw'
+global.youtube = 'https://youtube.com/channel/UCClQKwFKgIAiqJPgPpzGjmA'
 global.ig = 'https://instagram.com/yougod.12'
 global.mygc = 'https://chat.whatsapp.com/CRY23lVKmXB7sCMhHmcwVw'
-global.myweb = '@FF_BAGAS_STORE'
-global.email = 'bagasjb012@gmail.com'
+global.myweb = 'https://instagram.com/yougod.12'
+global.email = 'arwesputra@gmail.com'
 global.region = 'indonesia'
 
 //—————「 Set Wm 」—————//
-global.packname = '© BOTZ GASZ' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.author = 'BAGAS JB¹²' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.packname = '© Kurokawa Zaka'
+global.author = 'Arwes'
 
 //—————「 Set Nama Session 」—————//
-//gausah di apa² in!
 global.sessionName = 'session'
 
 //—————「 Set Limit 」—————//
-//terserah mau ubah atau nggak, note : tanda ' gausah di hapus!
 global.limitawal = {
 premium: "Infinity",
 free: 15,
 }
 
 //—————「 Set Image 」—————//
-//terserah mau ubah apa nggak, menurut gua jangan di ubah!
 global.thumb = fs.readFileSync('./image/chika.jpg')
-/*global.veloriy = fs.readFileSync('./image/chika.mp4')*/
+global.veloriy = fs.readFileSync('./image/chika.mp4')
 
 //—————「 Set Image Welcome 」—————//
-//terserah mau ubah apa nggak, menurut gua jangan di ubah!
 global.BgWelcLeav = 'https://telegra.ph/file/ca207893ae26d531cd9c6.jpg'
 
 //—————「 Set Random Image Menu 」—————//
-//gausah di apa² in!
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 
 //—————「 Set Prefix 」—————//
-//gausah di apa² in!
 global.prefa = ['','!','.','🐦','🐤','🗿']
 
 //—————「 Set Simbol 」—————//
-//terserah mai ubah atau nggak, note : tanda ' gausah di hapus!
 global.sp = '⭔'
 
 //—————「 Set Message 」—————//
-//terserah mau ubah apa nggak, note : tanda ' gausah di hapus!
 global.mess = {
 success: '_Done._',
 admin: '_Fitur Khusus Admin Group!_',
@@ -95,7 +81,6 @@ endLimit: '_Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12_',
 }
 
 //—————「 Batas Akhir 」—————//
-//gausah si apa² in!
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
 fs.unwatchFile(file)
